49b1f2423d9311d7e3271c8c04a4251b639dff03
Shrikant <sawantshrikant07@gmail.com>
2023-03-02 20:12:41 +0530

    [Shrikant] add : Greatest of 3 numbers

---
+++ Greatestno/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1 = 50;
+        int num2 = 100;
+        int num3 = 200;
+
+        int greatest = num1;
+        if (num2 > greatest)
+        {
+            greatest = num2;
+        }
+        if (num3 > greatest)
+        {
+            greatest = num3;
+        }
+
+        Console.WriteLine($"The greatest number is {greatest}.");
+    }
+}

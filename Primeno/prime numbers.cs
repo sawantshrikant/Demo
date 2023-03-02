d5f82981fcdcb46eab7ebd2dec10192da96665fb
Shrikant <sawantshrikant07@gmail.com>
2023-03-02 20:36:43 +0530

    [Shrikant] add : prime numbers between-0 to 100

---
+++ Primeno/prime numbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Primeno
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime numbers between 0 and 100:");
+
+            for (int i = 2; i <= 100; i++)
+            {
+                bool isPrime = true;
+
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
